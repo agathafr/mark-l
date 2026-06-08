d89dd690efc1d342827787290a67dbdfc5b9ffc4
Agatha França <agathafranca@outlook.com>
2026-06-08 17:59:07 -0300

    chore: add initial project structure
    Add the initial web application files and the E2E teste project setup. Include the fist test scenarios created during the initial study phase.

---
+++ cypress-express-mark/cypress/e2e/tasks.cy.js
@@ -0,0 +1,80 @@
+/// <reference types="cypress" />
+
+describe('tarefas', () => {
+
+    it('deve cadastrar uma nova tarefa', () => {
+
+        const taskName = 'Ler um livro de node.js'
+
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: { name: taskName }
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+
+        cy.visit('http://localhost:3000')
+
+        cy.get('input[placeholder="Add a new Task"]')
+            .type(taskName)
+
+        // //button[contains(text(), "Create")]
+
+        cy.contains('button', 'Create').click()
+
+        cy.contains('main div p', taskName)
+            .should('be.visible')
+    })
+
+    it('não deve permitir tarefa duplicada', () => {
+
+        const task = {
+            name: 'Estudar Javascript',
+            is_done: false
+        }
+
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: { name: task.name }
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+
+        // Dado que eu tenho uma tarefa duplicada
+
+        cy.request({
+            url: 'http://localhost:3333/tasks',
+            method: 'POST',
+            body: task
+        }).then(response => {
+            expect(response.status).to.eq(201)
+        })
+
+        // Quando faço o cadastro dessa tarefa
+        cy.visit('http://localhost:3000')
+
+        cy.get('input[placeholder="Add a new Task"]')
+            .type(task.name)
+
+        cy.contains('button', 'Create').click()
+
+        // cy.contains('.swal2-html-container', 'Task already exists!')
+        //     .should('be.visible')
+
+        // Então vejo a mensagem de duplicidade
+        cy.get('.swal2-html-container')
+            .should('be.visible')
+            .should('have.text', 'Task already exists!')
+    })
+})
+
+Cypress.Commands.add('createTask', (taskName) => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('input[placeholder="Add a new Task"]')
+        .type(taskName)
+
+    cy.contains('button', 'Create').click()
+})
