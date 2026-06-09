02ba2cdcdf1f6ef8354ef26dc1c2577521b74b10
Agatha França <agathafranca@outlook.com>
2026-06-09 15:29:03 -0300

    chore: adds context to the suite
    Groups the test cases related to the registration operation in the 'context' function within the 'describe' of the functionality targeted by the tests.

--- cypress-express-mark/cypress/e2e/tasks.cy.js
+++ cypress-express-mark/cypress/e2e/tasks.cy.js
@@ -2,36 +2,38 @@
 
 describe('tarefas', () => {
 
-    it('deve cadastrar uma nova tarefa', () => {
+    context('cadastro', () => {
+        it('deve cadastrar uma nova tarefa', () => {
 
-        const taskName = 'Ler um livro de node.js'
+            const taskName = 'Ler um livro de node.js'
 
-        cy.removeTaskByName(taskName)
-        cy.createTask(taskName)
+            cy.removeTaskByName(taskName)
+            cy.createTask(taskName)
 
-        cy.contains('main div p', taskName)
-            .should('be.visible')
-    })
+            cy.contains('main div p', taskName)
+                .should('be.visible')
+        })
 
-    it('não deve permitir tarefa duplicada', () => {
+        it('não deve permitir tarefa duplicada', () => {
 
-        const task = {
-            name: 'Estudar Javascript',
-            is_done: false
-        }
+            const task = {
+                name: 'Estudar Javascript',
+                is_done: false
+            }
 
-        cy.removeTaskByName(task.name)
-        cy.postTask(task)
-        cy.createTask(task.name)
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+            cy.createTask(task.name)
 
-        cy.get('.swal2-html-container')
-            .should('be.visible')
-            .should('have.text', 'Task already exists!')
-    })
+            cy.get('.swal2-html-container')
+                .should('be.visible')
+                .should('have.text', 'Task already exists!')
+        })
 
-    it('campo obrigatório', () => {
-        cy.createTask()
+        it('campo obrigatório', () => {
+            cy.createTask()
 
-        cy.isRequired('This is a required field')
+            cy.isRequired('This is a required field')
+        })
     })
 })
